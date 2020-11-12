e639bde7d7e47b0e05c090e0480a6cf7e55a73de
doawoo <woof@puppy.surf>
2020-11-12 09:30:30 -0800

    fix padding function

--- lib/util/util.ex
+++ lib/util/util.ex
@@ -1,12 +1,13 @@
 defmodule Macaroon.Util do
+  @spec truncate_or_pad_string(binary, number) :: binary
   def truncate_or_pad_string(str, target_len \\ 32) when is_binary(str) do
     cond do
-      String.length(str) > target_len ->
+      byte_size(str) > target_len ->
         {str, _rest} = String.split_at(str, target_len)
         str
-      String.length(str) < target_len ->
-        str <> String.duplicate("0", target_len - String.length(str))
-      String.length(str) == target_len ->
+      byte_size(str) < target_len ->
+        str <> String.duplicate(<<0>>, target_len - byte_size(str))
+      byte_size(str) == target_len ->
         str
     end
   end
