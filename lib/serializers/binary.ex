fd1f8f819cfea380ca2f3fc3021d8cee0efa48f9
Digit <woof@puppy.surf>
2021-05-05 08:35:33 -0700

    fix binary serialization and un-serialization

--- lib/serializers/binary.ex
+++ lib/serializers/binary.ex
@@ -10,38 +10,40 @@ defmodule Macaroon.Serializers.Binary do
   @spec encode(Macaroon.Types.Macaroon.t(), :v1) :: binary | {:error, any}
   def encode(%Types.Macaroon{} = macaroon, :v1) do
     with {:ok, location} <- create_packet_v1("location", macaroon.location),
-      {:ok, id} <- create_packet_v1("identifier", macaroon.public_identifier),
-      {:ok, sig} <- create_packet_v1("signature", macaroon.signature),
-      {:ok, caveats_encoded} <- encode_caveats_v1(macaroon) do
-        location <> id <> caveats_encoded <> sig
-        |> Base.url_encode64(padding: false)
-        |> String.replace("/", "_")
-      else
-        {:error, _} = err -> err
-      end
+         {:ok, id} <- create_packet_v1("identifier", macaroon.public_identifier),
+         {:ok, sig} <- create_packet_v1("signature", macaroon.signature),
+         {:ok, caveats_encoded} <- encode_caveats_v1(macaroon) do
+      (location <> id <> caveats_encoded <> sig)
+      |> Base.url_encode64(padding: false)
+    else
+      {:error, _} = err -> err
+    end
   end
 
   def decode(bin_macaroon, :v1) when is_binary(bin_macaroon) do
     {:ok, decoded} = Base.url_decode64(bin_macaroon, padding: false)
-    decode_packets_v1(decoded)
+    do_decode_macaroon_v1(decoded)
   end
 
   # Encoder v1 functions
 
   defp encode_caveats_v1(%Types.Macaroon{} = macaroon) do
     cavs = macaroon.first_party_caveats ++ macaroon.third_party_caveats
-    result = Enum.reduce_while(cavs, <<>>, fn caveat, packet ->
-       encoded = case caveat.party do
-        :first -> encode_first_party_caveat_v1(caveat)
-        :third -> encode_third_party_caveat_v1(caveat)
-       end
 
-       with {:ok, c_encoded} <- encoded do
-        {:cont, packet <> c_encoded}
-       else
-        {:error, _} = err -> {:halt, err}
-       end
-    end)
+    result =
+      Enum.reduce_while(cavs, <<>>, fn caveat, packet ->
+        encoded =
+          case caveat.party do
+            :first -> encode_first_party_caveat_v1(caveat)
+            :third -> encode_third_party_caveat_v1(caveat)
+          end
+
+        with {:ok, c_encoded} <- encoded do
+          {:cont, packet <> c_encoded}
+        else
+          {:error, _} = err -> {:halt, err}
+        end
+      end)
 
     if !match?({:error, _}, result) do
       {:ok, result}
@@ -50,14 +52,16 @@ defmodule Macaroon.Serializers.Binary do
     end
   end
 
-  defp create_packet_v1(key, data) when is_binary(key) and is_binary(data) do
+  def create_packet_v1(key, data) when is_binary(key) and is_binary(data) do
     p_size = @packet_prefix_len + @newline_and_space_len + byte_size(key) + byte_size(data)
+
     if p_size > @max_packet_size do
       {:error, "Packet size is too large for key #{key} and provided data"}
     else
-      packet_header = Integer.to_string(p_size, 16)
-      |> String.pad_leading(4, ["0"])
-      |> String.downcase()
+      packet_header =
+        Integer.to_string(p_size, 16)
+        |> String.pad_leading(4, ["0"])
+        |> String.downcase()
 
       packet_data = <<>> <> key <> " " <> data <> "\n"
       {:ok, packet_header <> packet_data}
@@ -70,8 +74,8 @@ defmodule Macaroon.Serializers.Binary do
 
   defp encode_third_party_caveat_v1(%Types.Caveat{} = c) do
     with {:ok, cid} <- create_packet_v1("cid", c.caveat_id),
-    {:ok, vid} <- create_packet_v1("vid", c.verification_key_id),
-    {:ok, location} <- create_packet_v1("cl", c.location) do
+         {:ok, vid} <- create_packet_v1("vid", c.verification_key_id),
+         {:ok, location} <- create_packet_v1("cl", c.location) do
       {:ok, cid <> vid <> location}
     else
       {:error, _} = err -> err
@@ -80,37 +84,75 @@ defmodule Macaroon.Serializers.Binary do
 
   # Decoder v1 functions
 
-  defp decode_packets_v1(decoded_bin) when is_binary(decoded_bin) do
-    packets = do_decode_packet_v1(decoded_bin, [])
+  defp do_decode_macaroon_v1(decoded_bin) when is_binary(decoded_bin) do
+    packets = do_decode_packets_v1(decoded_bin, []) |> Enum.reverse()
     base_mac = Types.Macaroon.build()
-    Enum.reduce(packets, base_mac, fn pkt, m ->
-      case String.split(pkt, " ", parts: 2) do
-        ["location", location] ->
-          %Types.Macaroon{m | location: location |> String.trim_trailing()}
-        ["identifier", id] ->
-          %Types.Macaroon{m | public_identifier: id |> String.trim_trailing()}
-        ["signature", sig] ->
-          %Types.Macaroon{m | signature: sig |> String.trim_trailing()}
-        pkt ->
-          do_decode_caveat_v1(pkt, m)
+    do_parse_packets_v1(packets, base_mac)
+  end
+
+  defp do_parse_packets_v1(packets, %Types.Macaroon{} = macaroon) when length(packets) > 0 do
+    [pkt | rest] = packets
+    next = List.first(rest) || ""
+
+    macaroon =
+      case pkt do
+        "location " <> location ->
+          %Types.Macaroon{macaroon | location: location |> String.trim_trailing()}
+
+        "identifier " <> id ->
+          %Types.Macaroon{macaroon | public_identifier: id |> String.trim_trailing()}
+
+        "signature " <> sig ->
+          %Types.Macaroon{macaroon | signature: sig |> String.trim_trailing()}
+
+        "cid " <> caveat_id ->
+          if !String.starts_with?(next, "vid ") do
+            c = Types.Caveat.build(caveat_id: caveat_id |> String.trim_trailing())
+            %Types.Macaroon{macaroon | first_party_caveats: [c | macaroon.first_party_caveats]}
+          else
+            c =
+              Types.Caveat.build(
+                party: :third,
+                caveat_id: caveat_id |> String.trim_trailing()
+              )
+
+            %Types.Macaroon{macaroon | third_party_caveats: [c | macaroon.third_party_caveats]}
+          end
+
+        "vid " <> verification_id ->
+          c = List.first(macaroon.third_party_caveats)
+          c = %Types.Caveat{c | verification_key_id: verification_id |> String.trim_trailing()}
+
+          %Types.Macaroon{
+            macaroon
+            | third_party_caveats: List.replace_at(macaroon.third_party_caveats, 0, c)
+          }
+
+        "cl " <> caveat_location ->
+          c = List.first(macaroon.third_party_caveats)
+          c = %Types.Caveat{c | location: caveat_location |> String.trim_trailing()}
+
+          %Types.Macaroon{
+            macaroon
+            | third_party_caveats: List.replace_at(macaroon.third_party_caveats, 0, c)
+          }
       end
-    end)
+
+    do_parse_packets_v1(rest, macaroon)
   end
 
-  defp do_decode_caveat_v1(pkt, %Types.Macaroon{} = macaroon) do
-    require IEx; IEx.pry
-    macaroon
-  end
+  defp do_parse_packets_v1(_packets, macaroon), do: macaroon
 
-  defp do_decode_packet_v1(bin, pkt_acc) when is_binary(bin) do
-    {len, rest} = String.split_at(bin, @packet_prefix_len)
+  defp do_decode_packets_v1(bin, pkt_acc) when is_binary(bin) do
+    len = binary_part(bin, 0, @packet_prefix_len)
     {len, _rest} = Integer.parse(len, 16)
-    {pkt, rest} = String.split_at(rest, len - 4)
+    pkt = binary_part(bin, @packet_prefix_len, len - @packet_prefix_len)
+    rest = binary_part(bin, len, byte_size(bin) - byte_size(pkt) - @packet_prefix_len)
 
     pkt_acc = [pkt | pkt_acc]
 
     if String.length(rest) > 0 do
-      do_decode_packet_v1(rest, pkt_acc)
+      do_decode_packets_v1(rest, pkt_acc)
     else
       pkt_acc
     end
--- test/binary_serializer_test.exs
+++ test/binary_serializer_test.exs
@@ -2,6 +2,7 @@ defmodule BinarySerializerTest do
   use ExUnit.Case
 
   alias Macaroon.Serializers
+  alias Macaroon.Util.Crypto
 
   @m_location "https://example.com"
   @m_id "1234"
@@ -16,15 +17,44 @@ defmodule BinarySerializerTest do
       assert m == decoded
     end
 
-    test "Should seralize a third party caveat" do
+    test "Should seralize a first party caveat" do
       m = Macaroon.create_macaroon(@m_location, @m_id, @m_secret)
       |> Macaroon.add_first_party_caveat("account = 1234")
-      |> Macaroon.add_third_party_caveat("location", "id", "key")
 
       encoded_string = Serializers.Binary.encode(m, :v1)
 
       decoded = Serializers.Binary.decode(encoded_string, :v1)
       assert m == decoded
+    end
+
+    test "Should seralize a third party caveat" do
+      static_nonce = Crypto.truncate_or_pad_string(<<0>>, :enacl.secretbox_NONCEBYTES)
+
+      m = Macaroon.create_macaroon(@m_location, @m_id, @m_secret)
+      |> Macaroon.add_third_party_caveat("http://auth.example.com", "account = 1234", "SECRET_KEY_TP", static_nonce)
+
+      encoded_string = Serializers.Binary.encode(m, :v1)
+
+      decoded = Serializers.Binary.decode(encoded_string, :v1)
+      assert m == decoded
+    end
+
+    test "Should fail to serialize a first party packet that is too long" do
+      m = Macaroon.create_macaroon(@m_location, @m_id, @m_secret)
+      |> Macaroon.add_first_party_caveat(String.pad_leading("a = ", 70000, "b"))
+
+      encoded_string = Serializers.Binary.encode(m, :v1)
+
+      assert {:error, _} = encoded_string
+    end
+
+    test "Should fail to serialize a third party packet that is too long" do
+      m = Macaroon.create_macaroon(@m_location, @m_id, @m_secret)
+      |> Macaroon.add_third_party_caveat(String.pad_leading("a = ", 70000, "b"), "id", "key")
+
+      encoded_string = Serializers.Binary.encode(m, :v1)
+
+      assert {:error, _} = encoded_string
     end
   end
 end
